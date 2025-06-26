6d49c73140623e24666d760e1b7d9a1019a2835c
Thiha Kyaw Htin <thihakyawhtin.mm@gmail.com>
2025-06-26 12:34:20 +0700

    create a new interface IOldPhoneKeyService to centralize the core logic

---
+++ OldPhone.ConsoleApp/IOldPhoneKeyService.cs
@@ -0,0 +1,17 @@
+﻿namespace OldPhone.ConsoleApp
+{
+    public interface IOldPhoneKeyService
+    {
+        string CurrentText { get; }
+
+        void ProcessKey(char key);
+
+        void ProcessBackspace();
+
+        void ProcessTimeout();
+
+        void ProcessComplete();
+
+        void ProcessCleaning();
+    }
+}
