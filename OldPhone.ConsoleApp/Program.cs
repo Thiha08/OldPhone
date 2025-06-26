789f5070f518e168e63da99b73e0856a9dfa0023
Thiha Kyaw Htin <thihakyawhtin.mm@gmail.com>
2025-06-26 07:16:49 +0700

    Add project files.

---
+++ OldPhone.ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OldPhone.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
