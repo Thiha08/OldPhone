564266223e51ddad52e42ebadee0cc81b8bcf620
Thiha Kyaw Htin <thihakyawhtin.mm@gmail.com>
2025-06-27 08:54:46 +0700

    implemented phone cmd help to read and validate user cmd input

--- OldPhone.ConsoleApp/Constants.cs
+++ OldPhone.ConsoleApp/Constants.cs
@@ -4,5 +4,13 @@
     {
         // pause for a second in order to type two characters  
         public const int TIMER_INTERVAL_MS = 1000;
+
+        // Regex pattern for valid phone keypad characters + special commands
+        // [1-9] = digits 1-9
+        // 0 = digit 0  
+        // [\s] = space character
+        // [*#] = special characters * and #
+        // [MOSQ] = special commands M, S, Q
+        public const string PHONE_KEY_REGEX = @"^[1-90*#MOQS\s]+$";
     }
 }
---
+++ OldPhone.ConsoleApp/PhoneCmdHelper.cs
@@ -0,0 +1,83 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace OldPhone.ConsoleApp
+{
+    public static class PhoneCmdHelper
+    {
+        private static readonly Regex PhoneKeypadPattern = new Regex(Constants.PHONE_KEY_REGEX, RegexOptions.Compiled);
+
+        /// <summary>
+        /// Reads input from console
+        /// </summary>
+        /// <returns>The valid input string</returns>
+        public static string ReadValidPhoneInput()
+        {
+            var input = new StringBuilder();
+            
+            while (true)
+            {
+                var key = Console.ReadKey(true);
+                var keyChar = key.KeyChar;
+                
+                if (key.Key == ConsoleKey.Enter)
+                {
+                    Console.WriteLine();
+                    return input.ToString();
+                }
+                
+                if (key.Key == ConsoleKey.Backspace)
+                {
+                    if (input.Length > 0)
+                    {
+                        input.Length--;
+                        Console.Write("\b \b");
+                    }
+                    continue;
+                }
+                
+                if (IsValidPhoneKeypadChar(keyChar))
+                {
+                    input.Append(keyChar);
+                    Console.Write(keyChar);
+                }
+
+                // Invalid key - ignore it
+            }
+        }
+
+        /// <summary>
+        /// Reads a single valid phone keypad character from console
+        /// </summary>
+        /// <returns>The valid character</returns>
+        public static char ReadValidPhoneKey()
+        {
+            while (true)
+            {
+                var key = Console.ReadKey(true);
+                var keyChar = key.KeyChar;
+                
+                if (IsValidPhoneKeypadChar(keyChar))
+                {
+                    Console.WriteLine(keyChar);
+                    return keyChar;
+                }
+                else
+                {
+                    // Invalid key - ignore it and continue waiting
+                    Console.Write("\b \b");
+                }
+            }
+        }
+
+        public static bool IsValidPhoneKeypadChar(char c)
+        {
+            return PhoneKeypadPattern.IsMatch(c.ToString());
+        }
+
+        public static string FilterValidCharacters(string input)
+        {
+            return new string(input.Where(IsValidPhoneKeypadChar).ToArray());
+        }
+    }
+}
