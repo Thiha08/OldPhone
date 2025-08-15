a82c6a66d443ce5de7a09bf4648d218c9dbbec61
Thiha Kyaw Htin <thihakyawhtin.mm@gmail.com>
2025-08-15 09:44:42 +0700

    multiapp structure update for ios and android

--- src/OldPhone.UI/OldPhone.UI.MultiApp/MauiProgram.cs
+++ src/OldPhone.UI/OldPhone.UI.MultiApp/MauiProgram.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using OldPhone.UI.MultiApp.Services;
 using OldPhone.UI.Shared.Services;
+using OldPhone.UI.Shared.Models.SignalR;
+using OldPhone.UI.Shared.Services.SignalR;
+using System.Reflection;
 
 namespace OldPhone.UI.MultiApp
 {
@@ -16,8 +20,19 @@ namespace OldPhone.UI.MultiApp
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 });
 
+            // Configure app settings
+            var assembly = Assembly.GetExecutingAssembly();
+            var configuration = new ConfigurationBuilder()
+                .AddJsonStream(assembly.GetManifestResourceStream("OldPhone.UI.appsettings.json")!)
+                .AddJsonStream(assembly.GetManifestResourceStream("OldPhone.UI.appsettings.Development.json")!)
+                .Build();
+
             // Add device-specific services used by the OldPhone.UI.Shared project
             builder.Services.AddSingleton<IFormFactor, FormFactor>();
+
+            builder.Services.AddSingleton<IConfiguration>(configuration);
+
+            builder.Services.AddSignalRClient(builder.Configuration);
 
             builder.Services.AddMauiBlazorWebView();
 
